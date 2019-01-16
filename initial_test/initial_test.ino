4702b631b7e1c8a29f5cb8e691f951b5b391b841
Yang Fang <yfang.msu@gmail.com>
2019-01-15 17:43:39 -0800

    a working version of feedback position control on pneumatic cylinder

--- initial_test/initial_test.ino
+++ initial_test/initial_test.ino
@@ -1,29 +1,83 @@
-#define DRIVE0_PIN 3
-#define DRIVE1_PIN A1
-#define DRIVE2_PIN A2
-#define DRIVE3_PIN A3
+#include <Event.h>
+#include <Timer.h>
+
+#include <AccelStepper.h>
+#include "Timer.h"
+
+// used pins
+#define MOTOR_EN         8  //D11
+#define MOTOR_L_PIN_DIR  4   //D6
+#define MOTOR_L_PIN_PUL  3   //D5
+#define LED              13
+#define THROTTLE_SPEED   (80000)
+#define THROTTLE_ACC     (8000000)
+#define PISTON_POS       A0
+#define P_PID_LIMIT      (200)
+#define DB               (0)
+
+AccelStepper stepper_left(AccelStepper::DRIVER, MOTOR_L_PIN_PUL, MOTOR_L_PIN_DIR);
+Timer monitor_timer;
+
+void rezero() {
+  digitalWrite(MOTOR_EN, HIGH);
+  stepper_left.setCurrentPosition(0);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-//  pinMode(DRIVE0_PIN, OUTPUT);
-  pinMode(DRIVE1_PIN, OUTPUT);
-  pinMode(DRIVE2_PIN, OUTPUT);
-  pinMode(DRIVE3_PIN, OUTPUT);
-  digitalWrite(DRIVE1_PIN, LOW);
-  digitalWrite(DRIVE2_PIN, LOW);
-  digitalWrite(DRIVE3_PIN, LOW);
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  pinMode(MOTOR_EN, OUTPUT);
+  digitalWrite(MOTOR_EN, HIGH);  //first thing: disable motor so no drive
+  delay(2000);
+  stepper_left.setMaxSpeed(THROTTLE_SPEED);
+  stepper_left.setAcceleration(THROTTLE_ACC);
+  rezero();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
-    // sets the value (range from 0 to 255):
-    analogWrite(DRIVE0_PIN, fadeValue);
-    // wait for 30 milliseconds to see the dimming effect
-    delay(400);
-//  digitalWrite(DRIVE0_PIN, HIGH);
-//  delay(20);
-//  digitalWrite(DRIVE0_PIN, LOW);
-//  delay(20);
+  int current_pos = 0;
+  int i = 0;
+  float desired_pos = 0;
+  float err_pos = 0;
+  long cmd_pos = 0;
+  float p_gain = 0.1;
+  float i_gain = 0.0001;
+  float i_pid = 0;
+  float p_pid = 0;
+
+  stepper_left.moveTo(0);
+  stepper_left.runToPosition();
+  delay(5000);
+    
+  while (true) {
+    desired_pos += 0.1;
+    if (desired_pos > 800) {
+      desired_pos = 0;
+    }
+    
+//    cmd_pos = 0;
+//    stepper_left.moveTo((long)cmd_pos);
+//    stepper_left.runToPosition();
+//    cmd_pos = -200;
+//    stepper_left.moveTo((long)cmd_pos);
+//    stepper_left.runToPosition();
+    
+    current_pos = analogRead(PISTON_POS);     // read the input pin
+    //Serial.println(current_pos);             // debug value
+    err_pos = -(float)(desired_pos - (float)current_pos);
+    i_pid += i_gain * err_pos;
+    p_pid = p_gain * err_pos;
+    if (p_pid > P_PID_LIMIT) {
+      p_pid = P_PID_LIMIT;
+    }
+    if (p_pid < -P_PID_LIMIT) {
+      p_pid = -P_PID_LIMIT;
+    }
+    cmd_pos = p_pid + i_pid;
+    stepper_left.moveTo((long)cmd_pos);
+    stepper_left.runToPosition();
+    delay(1);
+//    Serial.println(i_pid, "i_pid");
+//    Serial.println(p_pid);
   }
 }
